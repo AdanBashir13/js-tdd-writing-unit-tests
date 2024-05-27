feb850d867fba7c3969bce908d3507433d2f19d0
adan.bashir@student.moringaschool.com <adan.bashir@student.moringaschool.com>
2024-05-27 08:16:57 +0300

    Done with lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,13 @@
 // Your tests here
+import { pointsForWord } from "../utils.js";
+
+describe("pointsForWord", () => {
+    test("calculates the total points of a word (1 point per vowel, 2 points per consonant", () => {
+        const word = "tEsT";
+
+        const points = pointsForWord(word);
+
+        expect(points).toBe(7);
+
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
 // Your code here
+
+function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points;
+  }
+
+export { pointsForWord }
